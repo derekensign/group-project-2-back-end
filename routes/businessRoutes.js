e7af3e11b29cc7ab194243c24e35b9816b0aec67
Nick Link <nlink2021@gmail.com>
2021-04-15 11:13:39 -0500

    routes and controllers changed

--- routes/businessRoutes.js
+++ routes/businessRoutes.js
@@ -2,7 +2,8 @@ const businessController = require('../controllers/businessController')
 
 const express = require('express')
 const businessRoutes = express.Router()
-const axios = require('axios')
 
 businessRoutes.get('/', businessController.allBusinesses)
 businessRoutes.get('/:id', businessController.allBusinesses)
+
+module.exports = businessRoutes
